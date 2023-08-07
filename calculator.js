71e78c1a1e9bf6ec89ec96cb0831da4bdd0744fb
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-08-07 16:39:39 -0400

    added multiply functionality

--- calculator.js
+++ calculator.js
@@ -1,12 +1,47 @@
-let storage = 0;
+// declaring variables
+let storage = [];
+let operation;
 let screen = document.querySelector(".calc-screen");
 
-const buttons = document.querySelectorAll(".btn-number");
-
-buttons.forEach((button) => {
+// adding an event listener for each numbered button
+const numberedButtons = document.querySelectorAll(".btn-number");
+numberedButtons.forEach((button) => {
   button.addEventListener("click", function (event) {
     screen.innerText = button.innerText;
-    storage = screen.innerText;
+    storage.push(screen.innerText);
     console.log(storage);
   });
 });
+
+// adding an event listener for each operation button and storing operation into a variable
+const operationButtons = document.querySelectorAll(".btn-operation");
+operationButtons.forEach((button) => {
+  button.addEventListener("click", function (event) {
+    console.log(button);
+    if (button.classList.contains("multiply")) {
+      operation = "multiply";
+    } else if (button.classList.contains("divide")) {
+      operation = "divide";
+    } else if (button.classList.contains("add")) {
+      operation = "add";
+    } else if (button.classList.contains("minus")) {
+      operation = "minus";
+    } else if (button.classList.contains("equal")) {
+      result(storage, operation);
+    }
+  });
+});
+
+function result(storage, operation) {
+  console.log("this is my result function");
+  console.log(storage);
+  console.log(operation);
+  if (operation === "multiply") {
+    let answer = 1;
+    for (let i = 0; i < storage.length; i++) {
+      answer = answer * storage[i];
+    }
+    console.log(answer);
+    screen.innerText = answer;
+  }
+}
